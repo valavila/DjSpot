1212cbe31ce904d7603a1b7e589b7644ec545088
Valeria Avila <valeria.avila17@gmail.com>
2021-02-22 05:47:28 -0800

    Updated the register page to have user enter more personal info

--- DjSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DjSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,6 +52,10 @@ namespace DjSpot.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [StringLength(25)]
+            public string Username { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -61,7 +65,22 @@ namespace DjSpot.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+            public DateTime DOB { get; set; }
+
+            public string Bio { get; set; }
+
+            public bool isDj { get; set; }
+
+            public bool isCustomer { get; set; }
         }
+    
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -75,7 +94,18 @@ namespace DjSpot.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    DOB = Input.DOB,
+                    Bio = Input.Bio,
+                    isDj = Input.isDj,
+                    isCustomer = Input.isCustomer
+
+
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
